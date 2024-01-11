4f524c876521266ea982c397282ba6beae08dc3a
mi <sankawamidori@gmail.com>
2024-01-11 16:54:25 +1100

    feat: update homepage logo and images

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -39,7 +39,9 @@ const Home: FC = ()  => {
 
         {/* Note that all the cards need to be replaced with images */}
         <div className="pt-[150px] flex">
-          <PCard className="flex-1 h-[444px]"></PCard>
+          <PCard className="flex-1 h-[444px]">
+            <div className="h-[420px] mt-[-16px] bg-[url('src/assets/homepage_pic1.png')]"/>
+          </PCard>
           <div className="relative -top-2 flex-1">
             <div className="pl-24">
               <div className="w-[423px] text-[45px] font-bold font-playfair leading-[53px]">Describe your target group to distribute. We do the rest.</div>
@@ -53,11 +55,15 @@ const Home: FC = ()  => {
             <div className="w-[423px] my-[32px] text-[45px] font-bold font-playfair leading-[53px]">Your ideal participants are one click away.</div>
             <div className="w-[423px] text-[22px] font-normal font-playfair leading-[25px]"> Our algorithm pushes your research to the correct persons.<br></br>Get instant answers with real-time follow.No third-party involved.</div>
           </div>
-          <PCard className="flex-1 h-[444px] w-[666px]"></PCard>
+          <PCard className="flex-1 h-[444px] w-[666px] ">
+            <div className="w-[435px] h-[390px] ml-[50px] bg-[url('src/assets/homepage_pic2.png')]"/>
+          </PCard>
         </div>
 
         <div className="pt-[75px] flex">
-            <PCard className="flex-1 h-[444px]"></PCard>
+            <PCard className="flex-1 h-[444px] items-center">
+              <div className="w-[485px] h-[300px] mt-[36px] bg-[url('src/assets/homepage_pic3.png')]"/>
+            </PCard>
             <div className="relative -top-2 flex-1">
               <div className="pl-24">
                 <div className="w-[465px] my-[32px] text-[45px] font-bold font-playfair leading-[53px]">Monitor your research progress closely with your team.</div>
@@ -71,7 +77,9 @@ const Home: FC = ()  => {
             <div className="w-[423px] my-[32px] text-[45px] font-bold font-playfair leading-[53px]">You and your participants' data are safe with us.</div>
             <div className="w-[423px] text-[22px] font-normal font-playfair leading-[25px]"> Our data security algorithm safeguards your and your participants privacy.</div>
           </div>
-          <PCard className="flex-1 h-[444px]"></PCard>
+          <PCard className="flex-1 h-[444px]">
+            <div className="w-[605px] h-[270px] mt-[50px] ml-[-36px] bg-[url('src/assets/homepage_pic4.png')]"/>
+          </PCard>
         </div> 
       </div>
 
@@ -80,19 +88,13 @@ const Home: FC = ()  => {
         <div className="max-w-7xl mx-auto text-white">
 
           <div className="pt-[48px] flex justify-center">
-            {/* Bubble background image needs to be changed */}
-            <div className="pt-[16px] h-[160px] w-[940px] flex flex-col justify-center items-start bg-[url('src/assets/bg_circles2.svg')]">
-              <div className="font-playfair text-[52px] font-bold leading-[69.32px]">
-                Our offering
-              </div>
-              <div className="pl-[20px] font-playfair text-[40px] font-bold leading-[53.32px]">
-                Streamline your insight-gaining process.
-              </div>
-            </div>
+            <div className="pt-[16px] h-[160px] w-[940px] flex flex-col justify-center items-start bg-[url('src/assets/bg_circles3.svg')]"/>
           </div>
 
           <div className="pt-[150px] flex">
-            <PCard className="flex-1 h-[444px]"></PCard>
+            <PCard className="flex-1 h-[444px] bg-[#fff]">
+              <div className="w-[280px] h-[293px] mt-[50px] ml-[56px] bg-[url('src/assets/homepage_icon1.svg')]"/>
+            </PCard>
             <div className="relative -top-2 flex-1">
               <div className="pl-24">
                 <div className="w-[423px] text-[45px] font-bold font-playfair leading-[53px]">AI Assistant</div>
@@ -108,11 +110,15 @@ const Home: FC = ()  => {
               <div className="w-[423px] my-[32px] text-[45px] font-bold font-playfair leading-[53px]">AR Interview</div>
               <div className="w-[423px] text-[22px] font-normal font-playfair leading-[25px]"> Real and private dialogues.</div>
             </div>
-            <PCard className="flex-1 h-[444px] w-[666px]"></PCard>
+            <PCard className="flex-1 h-[444px] w-[666px] bg-[#fff]">
+              <div className="w-[306px] h-[308px] mt-[50px] ml-[186px] bg-[url('src/assets/homepage_icon2.svg')]"/>
+            </PCard>
           </div>
 
           <div className="pt-[75px] flex pb-[164px]">
-              <PCard className="flex-1 h-[444px]"></PCard>
+              <PCard className="flex-1 h-[444px] bg-[#fff]">
+               <div className="w-[300px] h-[300px] mt-[50px] ml-[56px] bg-[url('src/assets/homepage_icon3.svg')]"/>
+              </PCard>
               <div className="relative -top-2 flex-1">
                 <div className="pl-24">
                   <div className="w-[465px] my-[32px] text-[45px] font-bold font-playfair leading-[53px]">Data Analytics</div>
@@ -124,9 +130,8 @@ const Home: FC = ()  => {
         </div>
       </div>
 
-      <div className="max-w-7xl mx-auto h-[680px] pt-[48px] flex justify-center items-center">
-        <div className="w-[1096px] text-[115px] font-bold font-playfair">Book a Beta product Now.</div>
-        {/* Arrow logo */}
+      <div className="mx-auto h-[680px] pt-[48px] flex justify-center items-center">
+        <div className=" w-[1300px] h-[315px] bg-[url('src/assets/homepage_pic.png')]"/>
       </div>
     </div>
   )
